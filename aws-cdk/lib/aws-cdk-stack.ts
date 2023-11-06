d9a3267f90136a85701b1b5f043579c28f2d0612
Kubatbekkk <kubineccastro@gmail.com>
2023-11-06 16:55:20 +0600

    fix: add aws-cdk to react's .eslintignore

--- aws-cdk/lib/aws-cdk-stack.ts
+++ aws-cdk/lib/aws-cdk-stack.ts
@@ -1,19 +1,57 @@
-import { Duration, Stack, StackProps } from "aws-cdk-lib";
-import * as sns from "aws-cdk-lib/aws-sns";
-import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
-import * as sqs from "aws-cdk-lib/aws-sqs";
-import { Construct } from "constructs";
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3deploy from "@aws-cdk/aws-s3-deployment";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
 
-export class AwsCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class MyShopAppStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, "AwsCdkQueue", {
-      visibilityTimeout: Duration.seconds(300),
+    // Create a new S3 bucket
+    const bucket = new s3.Bucket(this, "MyShopAppBucket", {
+      versioned: false,
+      bucketName: "rs-cloud-shop-bucket",
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "index.html",
+      publicReadAccess: true,
     });
 
-    const topic = new sns.Topic(this, "AwsCdkTopic");
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(
+      this,
+      "OAI"
+    );
+    bucket.grantRead(originAccessIdentity);
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // Create a CloudFront distribution for the S3 bucket
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "MyShopAppDistribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: bucket,
+              originAccessIdentity,
+            },
+            behaviors: [{ isDefaultBehavior: true, compress: false }],
+          },
+        ],
+        errorConfigurations: [
+          {
+            errorCode: 404,
+            responsePagePath: "/index.html",
+            responseCode: 200,
+          },
+        ],
+      }
+    );
+
+    // Deploy site contents to S3 bucket
+    new s3deploy.BucketDeployment(this, "DeployWithInvalidation", {
+      sources: [s3deploy.Source.asset("../nodejs-aws-shop-react/dist")], // Update the directory accordingly
+      destinationBucket: bucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
   }
 }
