ee2a9d6e0de936e5bd1b011df166f56fab6d576d
Kubatbekkk <kubineccastro@gmail.com>
2023-11-25 15:28:35 +0600

    fix: small changes

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://w2jwx3924e.execute-api.us-east-1.amazonaws.com/prod",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://w2jwx3924e.execute-api.us-east-1.amazonaws.com/prod",
+  import: "https://w2jwx3924e.execute-api.us-east-1.amazonaws.com/prod",
+  bff: "https://w2jwx3924e.execute-api.us-east-1.amazonaws.com/prod",
+  cart: "https://w2jwx3924e.execute-api.us-east-1.amazonaws.com/prod",
 };
 
 export default API_PATHS;
