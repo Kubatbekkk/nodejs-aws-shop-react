c1a9991c2a03cd8afe719a61bc33668ac8ed7713
Kubatbekkk <kubineccastro@gmail.com>
2023-11-26 23:43:42 +0600

    fix: path id, feat: add count v2

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -7,39 +7,41 @@ import Typography from "@mui/material/Typography";
 import { formatAsPrice } from "~/utils/utils";
 import AddProductToCart from "~/components/AddProductToCart/AddProductToCart";
 import { useAvailableProducts } from "~/queries/products";
+import Chip from "@mui/material/Chip";
 
 export default function Products() {
-	const { data = [], isLoading } = useAvailableProducts();
+  const { data = [], isLoading } = useAvailableProducts();
 
-	if (isLoading) {
-		return <Typography>Loading...</Typography>;
-	}
+  if (isLoading) {
+    return <Typography>Loading...</Typography>;
+  }
 
-	return (
-		<Grid container spacing={4}>
-			{/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
-			{data.map(({ count, ...product }, index) => (
-				<Grid item key={product.id} xs={12} sm={6} md={4}>
-					<Card
-						sx={{ height: "100%", display: "flex", flexDirection: "column" }}
-					>
-						<CardMedia
-							sx={{ pt: "56.25%" }}
-							image={`https://source.unsplash.com/random?sig=${index}`}
-							title="Image title"
-						/>
-						<CardContent sx={{ flexGrow: 1 }}>
-							<Typography gutterBottom variant="h5" component="h2">
-								{product.title}
-							</Typography>
-							<Typography>{formatAsPrice(product.price)}</Typography>
-						</CardContent>
-						<CardActions>
-							<AddProductToCart product={product} />
-						</CardActions>
-					</Card>
-				</Grid>
-			))}
-		</Grid>
-	);
+  return (
+    <Grid container spacing={4}>
+      {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
+      {data.map(({ count, ...product }, index) => (
+        <Grid item key={product.id} xs={12} sm={6} md={4}>
+          <Card
+            sx={{ height: "100%", display: "flex", flexDirection: "column" }}
+          >
+            <CardMedia
+              sx={{ pt: "56.25%" }}
+              image={`https://source.unsplash.com/random?sig=${index}`}
+              title="Image title"
+            />
+            <CardContent sx={{ flexGrow: 1 }}>
+              <Typography gutterBottom variant="h5" component="h2">
+                {product.title}
+              </Typography>
+              <Typography>{formatAsPrice(product.price)}</Typography>
+              <Chip label={`count: ${count}`} />
+            </CardContent>
+            <CardActions>
+              <AddProductToCart product={product} />
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
