0ff4c5d30e49d3a41e887c9de8cd5aa904872513
Kubatbekkk <kubineccastro@gmail.com>
2024-01-07 12:45:07 +0600

    feat: change bff link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://w2jwx3924e.execute-api.us-east-1.amazonaws.com/prod",
   order: "http://kubatbekkk-cart-api-develop.us-west-2.elasticbeanstalk.com/api",
   import: "https://ffu12oew3l.execute-api.us-east-1.amazonaws.com/prod",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://tldkpfi496.execute-api.us-west-2.amazonaws.com/",
   cart: "http://kubatbekkk-cart-api-develop.us-west-2.elasticbeanstalk.com/api",
 };
 
