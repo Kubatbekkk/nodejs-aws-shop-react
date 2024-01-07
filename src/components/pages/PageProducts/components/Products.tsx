18bedf2f6320d94fb27b1077ee0974039560cb0a
Kubatbekkk <kubineccastro@gmail.com>
2024-01-07 14:32:56 +0600

    fix: change user

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -26,7 +26,9 @@ export default function Products() {
           >
             <CardMedia
               sx={{ pt: "56.25%" }}
-              image={`https://source.unsplash.com/random?sig=${index}`}
+              image={
+                product.image ? product.image : `https://source.unsplash.com/random?sig=${index}`
+              }
               title="Image title"
             />
             <CardContent sx={{ flexGrow: 1 }}>
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,8 +2,8 @@ const API_PATHS = {
   product: "https://w2jwx3924e.execute-api.us-east-1.amazonaws.com/prod",
   order: "http://kubatbekkk-cart-api-develop.us-west-2.elasticbeanstalk.com/api",
   import: "https://ffu12oew3l.execute-api.us-east-1.amazonaws.com/prod",
-  bff: "https://tldkpfi496.execute-api.us-west-2.amazonaws.com/",
-  cart: "https://tldkpfi496.execute-api.us-west-2.amazonaws.com/",
+  bff: "https://w2jwx3924e.execute-api.us-east-1.amazonaws.com/prod",
+  cart: "https://tldkpfi496.execute-api.us-west-2.amazonaws.com",
 };
 
 export default API_PATHS;
